ebed377c9d06fec98fd3e0eddc420a0fd05852a7
raphaelli <raphael_li@live.com>
2018-02-13 23:27:21 +0800

    add methods for getting todo items

--- TodoWebApi/Controllers/TodoController.cs
+++ TodoWebApi/Controllers/TodoController.cs
@@ -21,4 +21,21 @@ namespace TodoApi.Controllers
             }
         }       
     }
+
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAll()
+    {
+        return _context.TodoItems.ToList();
+    }
+
+    [HttpGet("{id}", Name = "GetTodo")]
+    public IActionResult GetById(long id)
+    {
+        var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        return new ObjectResult(item);
+    }
 }
