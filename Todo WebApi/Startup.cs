425240f0ae3be2ffb443130626a9a424cfbe6b84
raphaelli <raphael_li@live.com>
2018-02-13 22:53:14 +0800

    Register the database context

--- Todo WebApi/Startup.cs
+++ Todo WebApi/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Todo_WebApi.Models;
 
 namespace Todo_WebApi
 {
@@ -23,6 +25,7 @@ namespace Todo_WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc();
         }
 
