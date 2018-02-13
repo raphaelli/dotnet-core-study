a26fe6d9584382326114483f79adf6bd4f7cb01c
raphaelli <raphael_li@live.com>
2018-02-13 23:02:04 +0800

    reset

--- TodoWebApi/Startup.cs
+++ TodoWebApi/Startup.cs
@@ -8,8 +8,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
-using TodoWebApi.Models;
 
 namespace TodoWebApi
 {
@@ -25,7 +23,6 @@ namespace TodoWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc();
         }
 
