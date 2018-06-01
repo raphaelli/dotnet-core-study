844b4c154236125888fdf8f0b68f1262c52f7d87
raphaelli <raphael_li@live.com>
2018-06-02 01:32:10 +0800

    add gitignore and LTM.School init

---
+++ LTM.School/LTM.School/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LTM.School.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
